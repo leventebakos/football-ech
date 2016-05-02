00fab27929814a241b74265ded5acd25c73912d2
Levente Bakos <lbakos@hortonworks.com>
2016-05-02 22:14:17 +0200

    Add league app

---
+++ leagues/models.py
@@ -0,0 +1,17 @@
+from __future__ import unicode_literals
+from django.contrib.auth.models import User
+from django.db import models
+
+class League(models.Model):
+    league_name = models.CharField(max_length = 30)
+    max_size = models.IntegerField()
+    
+    def __str__(self):
+        return self.league_name
+    
+class LeagueParticipants(models.Model):
+    user = models.ForeignKey(User, models.CASCADE)
+    league = models.ForeignKey(League, models.CASCADE)
+    
+    def __str__(self):
+        return self.league.league_name + " - " + self.user.username
