d46a073c5498b9af06a1f1d559fef750ad30b884
Levente Bakos <lbakos@hortonworks.com>
2016-05-24 21:13:36 +0200

    Fix for weird characters

--- football_ech/wsgi.py
+++ football_ech/wsgi.py
@@ -7,10 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
+
+reload(sys)     
+sys.setdefaultencoding("utf-8")
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "football_ech.settings")
 
