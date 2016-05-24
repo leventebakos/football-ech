1afbfce895400e4efbadeb75402e4c4e2902e51e
Levente Bakos <lbakos@hortonworks.com>
2016-05-24 15:03:21 +0200

    Add group to match

--- matches/models.py
+++ matches/models.py
@@ -36,6 +36,7 @@ class Match(models.Model):
 	away_score = models.IntegerField(default = 0)
 	start_date = models.DateTimeField()
 	is_finished = models.BooleanField(default = False)
+	group = models.CharField(max_length = 20, null = True)
 
 	def __str__(self):
 		return self.get_home_team_display() + "-" + self.get_away_team_display() + ": " + str(self.home_score) + "-" + str(self.away_score) + "___" + str(self.start_date)
